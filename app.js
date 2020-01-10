ac8205dcf3d478264ba1f35a6fc1cca192adf6a5
Mohammed Junaid Abbas <abbas.m@hcl.com>
2020-01-10 22:16:50 +0100

    Change config for heroku

--- app.js
+++ app.js
@@ -30,10 +30,10 @@ app.use('/api', routes);
 // Handle Production
 if(process.env.NODE_ENV === 'production') {
   // Static folder
-  app.use(express.static(__dirname + '/public'));
+  app.use(express.static(__dirname + '/server/public'));
 
   // Handle SPA
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/server/public/index.html'));
 }
 
 app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
