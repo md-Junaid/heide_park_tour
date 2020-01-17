650812d9a72cc5f98c7dfc16df383d3ea8c55b3f
Mohammed Junaid Abbas <abbas.m@hcl.com>
2020-01-17 20:01:51 +0100

    Change config in webpack.prod.conf.js file to deploy

--- client/build/webpack.prod.conf.js
+++ client/build/webpack.prod.conf.js
@@ -32,7 +32,7 @@ const webpackConfig = merge(baseWebpackConfig, {
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
-      'process.env': env
+      'process.env.NODE_ENV': JSON.stringify('production')
     }),
     new UglifyJsPlugin({
       uglifyOptions: {
